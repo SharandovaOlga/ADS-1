7162dd20a537f4b6f1fc2be18830745e1c865110
SharandovaOlga <75723905+SharandovaOlga@users.noreply.github.com>
2021-05-31 20:02:33 +0300

    20-ИСТ-4 Шарандова О.К

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,22 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int r = 0, l = size - 1, k = 0;
+  while (r < l) {
+    int mid = (r + l) / 2;
+    if (arr[mid] < value)
+      r = mid + 1;
+            else
+      l = mid;
+    }
+    if (arr[r] == value) {
+      while (arr[r] == value) {
+        k++;
+         r++;
+      }
+    }
+    if (k != 0)
+      return k;
+            else
+      return 0; // если ничего не найдено
 }
